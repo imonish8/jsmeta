bd05c07a97183a88a06fe39d1adbb4c779f7fc65
Monish Nule <115737071+imonish8@users.noreply.github.com>
2024-02-17 12:07:50 +0530

    Create tryncatch.js
    Try Catch ERROR

---
+++ tryncatch.js
@@ -0,0 +1,13 @@
+console.log("This is explaination throw try and catch statements");
+try {
+  throw new error();
+}catch(err){
+  console.log(err);
+}
+console.log("this Line now Runs")
+//this line of code try and catch is used to handle the error
+
+//generally the tendency of java is to throw error and stop running right there without executing any code hence forth.
+//this is tackled by this try and catch error.
+
+
