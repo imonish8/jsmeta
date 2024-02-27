7bb7e4b9f32ae1a0b755c177ded08e2c88add235
Monish Nule <115737071+imonish8@users.noreply.github.com>
2024-02-27 20:43:24 +0530

    Create domAssign1.js

---
+++ domAssign1.js
@@ -0,0 +1,5 @@
+document.querySelector('p');
+document.querySelectorAll('p');
+document.querySelectorById(#);
+document.querySelectorByClassName(');
+//in short above code is used to select query in DOM Document Object Model., that is in HTML Web page or any webpage.
