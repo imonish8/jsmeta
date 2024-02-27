c153f414b19616dfaf75de07b63cc2841fdbf7ed
Monish Nule <115737071+imonish8@users.noreply.github.com>
2024-02-27 20:44:50 +0530

    Update domAssign1.js

--- domAssign1.js
+++ domAssign1.js
@@ -1,5 +1,5 @@
 document.querySelector('p');
 document.querySelectorAll('p');
-document.querySelectorById(#);
-document.querySelectorByClassName(');
+document.getElementById(#);
+document.getElementByClassName(');
 //in short above code is used to select query in DOM Document Object Model., that is in HTML Web page or any webpage.
